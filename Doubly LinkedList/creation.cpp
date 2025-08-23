1ec0a867adf6bde5223d214affa67069a540726a
Pallavi Govind Rao <govindraopallavi@gmail.com>
2025-08-24 03:07:17 +0530

    Update creation.cpp

--- Doubly LinkedList/creation.cpp
+++ Doubly LinkedList/creation.cpp
@@ -1,1 +1,29 @@
+#include<iostream>
+using namespace std;
+
+class Node{
+public:
+    int data;
+    Node* next;
+    Node* prev;
+
+    Node(int val){
+        data = val;
+        next = prev  = NULL;
+    }
+};
+
+class DoublyList{
+    Node* head;
+    Node* tail;
+
+public:
+    DoublyList(){
+        head = tail = NULL;
+    }
+};
+
+int main(){
+    DoublyList dll;
+}
 
